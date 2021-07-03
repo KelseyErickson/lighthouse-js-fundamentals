e14a618f9a1467c5293fa76423ff7b5fe5b2a6be
Kelsey Erickson <kerickson@gmx.com>
2021-07-03 15:00:04 +0000

    hello.js

--- hello.js
+++ hello.js
@@ -1,3 +1,4 @@
+
 const sayHello = function (name) {
   console.log('Hello, ' + name);
 }
@@ -5,3 +6,16 @@ const sayHello = function (name) {
 sayHello('Caliban');
 sayHello('Miranda');
 sayHello('Ferdinand');
+
+// using console.log
+const sayHelloToConsole = function(name){
+  console.log('Hello, ' + name);
+}
+sayHelloToConsole('John');
+
+//using return
+const returnSayHello = function (name) {
+  return 'Hello, ' + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
